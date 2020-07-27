61714312fd284add28906f810a9370acb424e466
antoinePaulinB7 <apaulinb7@gmail.com>
2020-07-27 18:18:44 -0400

    putting back the right directories

--- mainProject.cpp
+++ mainProject.cpp
@@ -2310,9 +2310,9 @@ int main(int argc, char* argv[])
   metal.lightColor = vec3(1.0f, 1.0f, 0.0f);
 
 	// Compile and link shaders here ...
-	defaultShaderProgram = shader("modelShader.vs", "modelShader.fs");
-	phongLightShaderProgram = shader("lightShader.vs", "lightShader.fs");
-	shadowShaderProgram = shader("shadowShader.vs", "shadowShader.fs");
+	defaultShaderProgram = shader("../Source/COMP371-Group14-Project/modelShader.vs", "../Source/COMP371-Group14-Project/modelShader.fs");
+	phongLightShaderProgram = shader("../Source/COMP371-Group14-Project/lightShader.vs", "../Source/COMP371-Group14-Project/lightShader.fs");
+	shadowShaderProgram = shader("../Source/COMP371-Group14-Project/shadowShader.vs", "../Source/COMP371-Group14-Project/shadowShader.fs");
 
 
 #pragma endregion windowSetUp
@@ -2327,7 +2327,7 @@ int main(int argc, char* argv[])
 	int gridVAO = createVertexArrayObjectGridLine();
 	int gridSquare = createGridSquareVertexArrayObject();
 	int xyzVAO = createVertexArrayObjectCoordinateXYZ();
-	int sphereVAO = createSphereObjectVAO("sphere.obj", sphereVertices);
+	int sphereVAO = createSphereObjectVAO("../Source/COMP371-Group14-Project/sphere.obj", sphereVertices);
 
 
 	//Create hierarchical models
