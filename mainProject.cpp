b0586adc4bf1e355e7865a3b95fbeece22e76506
Nicole Parmentier <nclparmentier@gmail.com>
2020-07-27 15:17:20 -0400

    added white color values for the sphere so its VAO structure matches the expectations of our light vertex shader

--- mainProject.cpp
+++ mainProject.cpp
@@ -318,11 +318,12 @@ int sphereVertices;
 
 GLuint createSphereObjectVAO(string path, int& vertexCount) {
 	std::vector<glm::vec3> vertices;
+	std::vector<glm::vec3> colors;
 	std::vector<glm::vec3> normals;
 	std::vector<glm::vec2> UVs;
 
 	//read the vertex data from the model's OBJ file
-	loadOBJ(path.c_str(), vertices, normals, UVs);
+	loadOBJ(path.c_str(), vertices, colors, normals, UVs);
 
 	GLuint VAO;
 	glGenVertexArrays(1, &VAO);
@@ -337,21 +338,29 @@ GLuint createSphereObjectVAO(string path, int& vertexCount) {
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 
+	//Colors VBO setup
+	GLuint colors_VBO;
+	glGenBuffers(1, &colors_VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, colors_VBO);
+	glBufferData(GL_ARRAY_BUFFER, colors.size() * sizeof(glm::vec3), &colors.front(), GL_STATIC_DRAW);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(1);
+
 	//Normals VBO setup
 	GLuint normals_VBO;
 	glGenBuffers(1, &normals_VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, normals_VBO);
 	glBufferData(GL_ARRAY_BUFFER, normals.size() * sizeof(glm::vec3), &normals.front(), GL_STATIC_DRAW);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
-	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(2);
 
 	//UVs VBO setup
 	GLuint uvs_VBO;
 	glGenBuffers(1, &uvs_VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, uvs_VBO);
 	glBufferData(GL_ARRAY_BUFFER, UVs.size() * sizeof(glm::vec2), &UVs.front(), GL_STATIC_DRAW);
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), (GLvoid*)0);
-	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(3, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(3);
 
 	glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs, as we are using multiple VAOs)
 	vertexCount = vertices.size();
