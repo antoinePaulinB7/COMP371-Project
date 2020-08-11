dfe2cc2faa111a3e82a2115902b84e88bdd715bd
Nicole Parmentier <nclparmentier@gmail.com>
2020-08-11 07:49:41 -0400

    Use in-out blocks for light variables within shaders

--- lightShader.fs
+++ lightShader.fs
@@ -2,20 +2,34 @@
   in vec2 texCoord;
 	in vec3 vertexColor;
     in vec3 normalN;
-    in vec3 lightVectorL;
     in vec3 eyeVectorV;
-    in float distanceToLightSource;
 	in vec3 fragPosition;
 
-	in vec4 shadowCoordinate;
 
-	uniform float shouldRenderShadows = 1.0f;
-  uniform float shouldRenderTextures = 1.0f;
+	in light1 {
+		vec3 lightVectorL;
+		vec4 shadowCoordinate;
+		float distanceToLightSource;
+	} light1;
+	uniform float isLight1On = 1.0f;
 	uniform sampler2D shadowMap;
-  uniform sampler2D someTexture;
 
-  uniform vec4 lightCoefficients = vec4(0.3f, 0.8f, 0.5f, 256);
-  uniform vec3 lightColor = vec3(1.0f, 1.0f, 1.0f);
+	in light2 {
+		vec3 lightVectorL;
+		vec4 shadowCoordinate;
+		float distanceToLightSource;
+	} light2;
+	uniform float isLight2On = 1.0f;
+	uniform sampler2D shadowMap2;
+
+	
+	uniform float shouldRenderShadows = 1.0f;
+	uniform float shouldRenderTextures = 1.0f;
+	uniform sampler2D someTexture;	
+
+    uniform vec4 lightCoefficients = vec4(0.3f, 0.8f, 0.5f, 256);
+    uniform vec3 lightColor = vec3(1.0f, 1.0f, 1.0f);
+
 	float coeffAmbient;
 	float coeffDiffuse;
 	float coeffSpecular;
@@ -24,84 +38,118 @@
 	out vec4 fragmentColor;
 
 		float clampIt(float value) {
-		if (value < 0.0f) {
-		return 0.0f;
-		} else if (value > 1.0f) {
-			return 1.0f;
-		} else {
-			return value;
+			if (value < 0.0f) {
+			return 0.0f;
+			} else if (value > 1.0f) {
+				return 1.0f;
+			} else {
+				return value;
+			}
 		}
-	}
+
+		float getVisibility(vec4 shadowCoordinates, float bias, int percentageCloserFilteringRadius, sampler2D shadowMapToUse) {
+			vec3 shadowCoord = (shadowCoordinates.xyz / shadowCoordinates.w) * 0.5 + 0.5; 
+			float visibility = 0.0f;
+			float dist = shadowCoord.z;
+
+			vec2 shadowMapDimensions = textureSize(shadowMapToUse, 0);	//https://www.khronos.org/opengl/wiki/Sampler_(GLSL)#Texture_size_retrieval
+			float shadowMapTexelWidth = 1.0f / shadowMapDimensions.x;
+			float shadowMapTexelHeight = 1.0f / shadowMapDimensions.y;
+			for (int i = -percentageCloserFilteringRadius; i <= percentageCloserFilteringRadius; ++i) {
+				for (int j = -percentageCloserFilteringRadius; j <= percentageCloserFilteringRadius; ++j) {
+					float depthVal = texture(shadowMapToUse, vec2(shadowCoord.x + (i*shadowMapTexelWidth), shadowCoord.y + (j*shadowMapTexelHeight))).z;
+					if (dist <= depthVal + bias) {
+						visibility += 1.0f;
+					}
+				}
+			}
+			float sampleDiameter = (percentageCloserFilteringRadius * 2.0f + 1.0f);
+			return visibility / (sampleDiameter * sampleDiameter);
+		}
+
+		vec3 getDiffuseIntensity(float attenuationFactor, vec3 norm, vec3 lightDirection) {
+			float diff = max(dot(norm, lightDirection), 0.0f);
+			return attenuationFactor * diff;
+		}
+
+		vec3 getSpecularIntensity(float attenuationFactor, vec3 norm, vec3 lightDirection) {
+			float coeffShininess = lightCoefficients[3]; //256;
+			vec3 viewDirection = normalize(eyeVectorV);
+			vec3 reflectDirection = reflect(-lightDirection, norm);
+			float spec = pow(max(dot(viewDirection, reflectDirection), 0.0), coeffShininess);
+			//eliminate the backface specular light https://stackoverflow.com/questions/20008089/specular-lighting-appears-on-both-eye-facing-and-rear-sides-of-object
+			if (dot(viewDirection, norm) < 0.0f) {
+				spec = 0.0f; 
+			}
+			return attenuationFactor * spec;
+		}
+
+		vec3 getTotalIntensity(float distToLightSource, vec3 lightVector, vec4 shadowCoordinate, float bias, int percentageCloserFilteringRadius, sampler2D shadowMap, bool isLightOn, float angle) {
+			//Initialize variables for lighting attributes
+			float coeffAmbient = lightCoefficients[0]; //0.3f;
+			float coeffDiffuse = lightCoefficients[1]; //0.8f;
+			float coeffSpecular = lightCoefficients[2]; //0.5f;
+			float coeffShininess = lightCoefficients[3]; //256;
+			
+			float visibility = getVisibility(shadowCoordinate, bias, percentageCloserFilteringRadius, shadowMap);
+
+			//attenuation maths following the attenuation formula Id = (kd*Ld) / (a + bq + cq^2) * (l dot n) attenuationFactor being 1/(a + bq + cq^2)
+			float attenuationConstantA = 0.5f;
+			float attenuationConstantB = 0.01f;
+			float attenuationConstantC = 0.0001f;
+			float attenuationFactor = 1 / (attenuationConstantA + (attenuationConstantB * distToLightSource) + (attenuationConstantC * distToLightSource * distToLightSource));
+			
+			vec3 norm = normalize(normalN);
+			vec3 lightDirection = normalize(lightVector - fragPosition);
+
+			vec3 ambientIntensity = lightColor * coeffAmbient;
+			vec3 diffuseIntensity = (coeffDiffuse * lightColor) * getDiffuseIntensity(attenuationFactor, norm, lightDirection);
+			vec3 specularIntensity = (coeffSpecular * lightColor) * getSpecularIntensity(attenuationFactor, norm, lightDirection);
+		
+			//Phong Lighting Model combines the 3 lighting components
+			if (shouldRenderShadows < 0.5f) {
+				visibility = 1.0f;
+			} 
+
+			//Only for use with spotlights
+			//float spotLitness = dot(lightDirection, normalize(lightVector));
+			//spotLitness = (spotLitness > angle ? spotLitness : 0.0f);
+			float lightOnness = 1.0f;
+			if (!isLightOn) {
+				lightOnness = 0.0f;
+				visibility = 0.0f;	//do not cast shadows when the light is off
+			} else {
+				//Only for use with spotlights
+				//spotLitness = clampIt(spotLitness);	
+				//spotLitness = (spotLitness - angle) * 10.0f;
+				//lightOnness = clampIt(spotLitness);	
+				//if (spotLitness <= bias) {
+				//	visibility = 0.0f;
+				//}
+			}
+
+			return ambientIntensity + ((visibility + lightOnness)/2.0f) * (diffuseIntensity + specularIntensity);
+		}
 
 	void main()
 	{
-		//Initialize variables for lighting attributes
-		float coeffAmbient = lightCoefficients[0]; //0.3f;
-		float coeffDiffuse = lightCoefficients[1]; //0.8f;
-		float coeffSpecular = lightCoefficients[2]; //0.5f;
-		float coeffShininess = lightCoefficients[3]; //256;
-		
+
 		//Shadow math
-		vec3 shadowCoord = (shadowCoordinate.xyz / shadowCoordinate.w) * 0.5 + 0.5; 
-		float visibility = 0.0f;
-		float dist = shadowCoord.z;
-		float bias = 0.00001f;
+		float bias = 0.000001f;
 		int percentageCloserFilteringRadius = 2;
-		vec2 shadowMapDimensions = textureSize(shadowMap, 0);	//https://www.khronos.org/opengl/wiki/Sampler_(GLSL)#Texture_size_retrieval
-		float shadowMapTexelWidth = 1.0f / shadowMapDimensions.x;
-		float shadowMapTexelHeight = 1.0f / shadowMapDimensions.y;
-		for (int i = -percentageCloserFilteringRadius; i <= percentageCloserFilteringRadius; ++i) {
-			for (int j = -percentageCloserFilteringRadius; j <= percentageCloserFilteringRadius; ++j) {
-				float depthVal = texture(shadowMap, vec2(shadowCoord.x + (i*shadowMapTexelWidth), shadowCoord.y + (j*shadowMapTexelHeight))).z;
-				if (dist <= depthVal + bias) {
-					visibility += 1.0f;
-				}
-			}
+
+		float angle = 0.76f; //0.76f is roughly cos(40), our spotlight covers 40 degrees
+		vec3 intensity1 = getTotalIntensity(light1.distanceToLightSource, light1.lightVectorL, light1.shadowCoordinate, bias, percentageCloserFilteringRadius, shadowMap, isLight1On > 0.5f, angle);
+		vec3 intensity2 = vec3(0.0f);
+		if (light2.lightVectorL != vec3(0.0f)) {
+			intensity2 = getTotalIntensity(light2.distanceToLightSource, light2.lightVectorL, light2.shadowCoordinate, bias, percentageCloserFilteringRadius, shadowMap2, isLight2On > 0.5f, angle);
 		}
-		float sampleDiameter = (percentageCloserFilteringRadius * 2.0f + 1.0f);
-		visibility = visibility / (sampleDiameter * sampleDiameter);
+		vec3 totalIntensity = intensity1 + intensity2;	
+		vec4 texColor = vec4(1.0f);
 
-
-		//ambient
-		vec3 ambientIntensity = lightColor * coeffAmbient;
-
-		//attenuation maths following the attenuation formula Id = (kd*Ld) / (a + bq + cq^2) * (l dot n) attenuationFactor being 1/(a + bq + cq^2)
-		float attenuationConstantA = 0.5f;
-		float attenuationConstantB = 0.01f;
-		float attenuationConstantC = 0.0001f;
-		float attenuationFactor = 1 / (attenuationConstantA + (attenuationConstantB * distanceToLightSource) + (attenuationConstantC * distanceToLightSource * distanceToLightSource));
-
-		//diffuse
-		vec3 norm = normalize(normalN);
-		vec3 lightDirection = normalize(lightVectorL - fragPosition);
-		float diff = max(dot(norm, lightDirection), 0.0f);
-		vec3 diffuseIntensity = attenuationFactor * (coeffDiffuse * lightColor) * diff;
-		
-		//specular
-		vec3 viewDirection = normalize(eyeVectorV);
-		vec3 reflectDirection = reflect(-lightDirection, norm);
-		float spec = pow(max(dot(viewDirection, reflectDirection), 0.0), coeffShininess);
-		//eliminate the backface specular light https://stackoverflow.com/questions/20008089/specular-lighting-appears-on-both-eye-facing-and-rear-sides-of-object
-		if (dot(viewDirection, norm) < 0.0f) {
-			spec = 0.0f; 
+		if(shouldRenderTextures > 0.5f) {
+		  texColor = texture(someTexture, texCoord);
 		}
-		vec3 specularIntensity = attenuationFactor * coeffSpecular * spec * lightColor;
-		
-		//Phong Lighting Model combines the 3 lighting components
-		vec3 totalIntensity;
-		if (shouldRenderShadows > 0.5f) {
-			totalIntensity = ambientIntensity + (visibility * diffuseIntensity) + (visibility * specularIntensity);
-		} else {
-			 totalIntensity = ambientIntensity + diffuseIntensity + specularIntensity;
-		}
-
-    vec4 texColor;
-
-    if(shouldRenderTextures > 0.5f) {
-      texColor = texture(someTexture, texCoord);
-    } else {
-      texColor = vec4(vertexColor.r, vertexColor.g, vertexColor.b, 1.0f);
-    }
 
 		//Calculate each color channel  (R,G,B) separately
 		//Clamp the final result to [0, 1]
@@ -109,8 +157,8 @@
 		float totalIntensityG = totalIntensity.g;//clampIt(totalIntensity.g);	
 		float totalIntensityB = totalIntensity.b;//clampIt(totalIntensity.b);	
 
-    fragmentColor = vec4(texColor.r * totalIntensityR, 
-      texColor.g * totalIntensityG, 
-      texColor.b * totalIntensityB,
+    fragmentColor = vec4(texColor.r * totalIntensityR * vertexColor.r, 
+      texColor.g * totalIntensityG * vertexColor.g, 
+      texColor.b * totalIntensityB * vertexColor.b,
       1.0f);
 	}
--- lightShader.vs
+++ lightShader.vs
@@ -1,24 +1,37 @@
 #version 330 core
     layout (location = 0) in vec3 aPos; 
-	  layout (location = 1) in vec3 aColor;
-	  layout (location = 2) in vec3 aNormal;
+	layout (location = 1) in vec3 aColor;
+	layout (location = 2) in vec3 aNormal;
     layout (location = 3) in vec2 aTex;
 	
     uniform vec3 cameraPosition;
     uniform mat4 worldMatrix;
     uniform mat4 viewMatrix = mat4(1.0);
     uniform mat4 projectionMatrix = mat4(1.0);
+
     uniform mat4 depthVP;
     uniform vec3 lightPosition;
+    
+	uniform mat4 depthVP2;
+    uniform vec3 lightPosition2 = vec3(0.0f);
 
     out vec2 texCoord;
     out vec3 vertexColor;
     out vec3 normalN;
     out vec3 fragPosition;
-    out vec3 lightVectorL;
     out vec3 eyeVectorV;
-    out vec4 shadowCoordinate;
-	out float distanceToLightSource;
+
+	out light1 {
+		vec3 lightVectorL;
+		vec4 shadowCoordinate;
+		float distanceToLightSource;
+	} light1;
+
+	out light2 {
+		vec3 lightVectorL;
+		vec4 shadowCoordinate;
+		float distanceToLightSource;
+	} light2;
 
     void main()
     {
@@ -33,14 +46,16 @@
 		//Light math
 		fragPosition = vec3(worldMatrix * vec4(aPos, 1.0));
 		normalN = vec3(worldMatrix * vec4(aNormal, 0.0f));
-		lightVectorL = lightPosition;
 		eyeVectorV = normalize(vec3(vec4((cameraPosition - fragPosition), 0.0f)));
+		
 
-		//Distance to Light math
+		light1.lightVectorL = lightPosition;
 		vec3 vectorToLightSource = lightPosition - fragPosition;
-		distanceToLightSource = length(vectorToLightSource);
+		light1.distanceToLightSource = length(vectorToLightSource);
+		light1.shadowCoordinate = depthVP * worldMatrix * vec4(aPos, 1.0f);
 
-		//Shadow math
-		//Transform points in shadow map
-		shadowCoordinate = depthVP * worldMatrix * vec4(aPos, 1.0f);
+		light2.lightVectorL = lightPosition2;
+		vec3 vectorToLightSource2 = lightPosition2 - fragPosition;
+		light2.distanceToLightSource = length(vectorToLightSource2);
+		light2.shadowCoordinate = depthVP2 * worldMatrix * vec4(aPos, 1.0f);
     }
