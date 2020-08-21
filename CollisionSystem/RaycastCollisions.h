a03cd3fb9d40f97f40989aa37afd5ca08385a5de
Briana Toia <55514975+bbtoia@users.noreply.github.com>
2020-08-21 15:05:47 -0400

    organized folders

---
+++ CollisionSystem/RaycastCollisions.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/common.hpp>
+#include <GL/glew.h>
+#include <vector>
+#include "Models/Model.h"
+
+using namespace std;
+using namespace glm;
+
+//Adapted from the provided collisions lab code by Zachary Lapointe
+bool doRaycastCollision(mat4 cameraViewMatrix, std::vector<Model*> modelsWithCollisions) {
+	glm::mat4 inverseView = glm::inverse(cameraViewMatrix);
+	glm::vec3 rayOrigin = inverseView[3]; //the camera position
+	glm::vec3 rayDirection = -1.0f * glm::normalize(inverseView[2]); //the camera lookat
+
+	//Try intersecting ray with every model
+	//We are shooting a ray from the camera, directly forward
+	float closestIntersection = std::numeric_limits<float>::infinity();
+	Model* intersectingModel = nullptr;
+	for (vector<Model*>::iterator it = modelsWithCollisions.begin(); it < modelsWithCollisions.end(); ++it)
+	{
+		Model* currentTarget = *it;
+		float intersectionPoint = currentTarget->IntersectsRay(rayOrigin, rayDirection);
+		if (intersectionPoint >= 0.0f && intersectionPoint < closestIntersection) {
+			closestIntersection = intersectionPoint;
+			intersectingModel = currentTarget;
+		}
+
+	}
+
+	if (intersectingModel != nullptr)
+	{
+		glm::vec3 intersectingPoint = rayOrigin + closestIntersection * rayDirection;
+		if (closestIntersection < 3.0f) {
+			return true;
+		}
+	}
+
+	return false;
+}
